3917a50871dabc525d702923994727197a49d91e
Vipul1705 <vipulpatil1999@gmail.com>
2021-07-19 14:32:24 +0530

    edits

--- intern/settings.py
+++ intern/settings.py
@@ -147,7 +147,7 @@ AUTHENTICATION_BACKENDS = (
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # WHITENOISE_USE_FINDERS = True
 
